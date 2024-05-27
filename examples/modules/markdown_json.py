a0d3eac0398f1c994584863db52ff1db672da699
PascalSun <pascalsun23@gmail.com>
2024-05-27 18:23:15 +0800

    update markdown

--- examples/modules/markdown_json.py
+++ examples/modules/markdown_json.py
@@ -1,11 +1,5 @@
-from Docs2KG.modules.markdown2json import Markdown2JSON
-from Docs2KG.parser.pdf.pdf2blocks import PDF2DataFrame
-from Docs2KG.parser.pdf.pdf2images import PDF2Images
-from Docs2KG.parser.pdf.pdf2metadata import PDF_TYPE_SCANNED, get_scanned_or_exported
-from Docs2KG.parser.pdf.pdf2tables import PDF2Tables
-from Docs2KG.parser.pdf.pdf2text import PDF2Text
+from Docs2KG.modules.native.markdown2json import Markdown2JSON
 from Docs2KG.utils.constants import DATA_OUTPUT_DIR
-from Docs2KG.utils.get_logger import get_logger
 
 if __name__ == "__main__":
     input_md_file = DATA_OUTPUT_DIR / "4.pdf" / "4.md"
