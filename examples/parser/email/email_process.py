2605d12946c2e14305fdd409b7b58f3de3d634a4
PascalSun <pascalsun23@gmail.com>
2024-06-02 20:31:08 +0800

    block the email

--- examples/parser/email/email_process.py
+++ examples/parser/email/email_process.py
@@ -1,13 +1,11 @@
 from Docs2KG.parser.email.email2images import Email2Images
 from Docs2KG.parser.email.email2markdown import Email2Markdown
+from Docs2KG.utils.constants import DATA_INPUT_DIR
 
-# from Docs2KG.parser.email.email2table import Email2Table
 
 if __name__ == "__main__":
-    email_filename = "UWA Sport eNews _ April 2024.eml"
+    email_filename = DATA_INPUT_DIR / "email" / "email.eml"
     email2md = Email2Markdown(email_filename=email_filename)
     email2md.convert2markdown()
     email2images = Email2Images(email_filename=email_filename)
     email2images.extract2images()
-    # email2table = Email2Table(email_filename=email_filename)
-    # email2table.extract2table()
