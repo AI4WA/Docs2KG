81ccc864fab64bd68947c18b7299d27bfb39023d
PascalSun <pascalsun23@gmail.com>
2024-06-04 11:47:32 +0800

    update RAG example

--- examples/rag/neo4j_retrieval.py
+++ examples/rag/neo4j_retrieval.py
@@ -9,11 +9,14 @@ if __name__ == "__main__":
     password = "testpassword"
 
     rag = Neo4jRAG(uri, username, password)
-    query = "What is the capital of France?"
+    query = (
+        "Can you show me all documents and their components"
+        "related to events that occurred in the years 2011 and 2021?"
+    )
     result = rag.retrieval(query)
     logger.info(result)
 
     rag.retrieval_strategy_hops_away(
         uuids=result["top_k_content"],
-        hops=2,
+        hops=3,
     )
