42caaa01c51f3a02a56b20f1bc9065012eb17e24
PascalSun <pascalsun23@gmail.com>
2025-01-04 15:55:53 +0800

    add metadata kg extraction

--- examples/kg/layout_kg.py
+++
@@ -1,8 +0,0 @@
-from Docs2KG.kg.pdf_layout_kg import PDFLayoutKG
-from Docs2KG.utils.constants import DATA_OUTPUT_DIR
-
-if __name__ == "__main__":
-    input_folder = DATA_OUTPUT_DIR / "MESAJ084.pdf"
-
-    layout_kg = PDFLayoutKG(input_folder)
-    layout_kg.create_kg()
